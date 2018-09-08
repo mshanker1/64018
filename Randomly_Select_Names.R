8dde8ee76bc9bd9e8f7d52b4fc5dbb1ab8bf493b
Murali Shanker <mshanker@Muralis-MacBook-Air.local>
2018-09-07 22:04:04 -0400

    added random selection

---
+++ Randomly_Select_Names.R
@@ -0,0 +1,18 @@
+
+a <- read.csv("~/Git/64018/names.csv", sep="", stringsAsFactors=FALSE)
+names <- as.matrix(a) # so dealing with a character array
+gs = 2 # group size
+
+
+myfun <- function(ln, ss) {
+  fs1 <- sample(ln,ss,replace=F)
+  fs1
+}
+
+while (length(names) > gs) {
+  ns <- myfun(names, gs)
+  print(ns)
+  names <- setdiff(names,ns);
+ }
+
+print(names)
